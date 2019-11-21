7914e722fb566965d8903b26decc4a4d8cf3ce67
jacobdjake <57650461+jacobdjake@users.noreply.github.com>
2019-11-21 20:56:20 +0800

    Update names.js

--- family_tree/names.js
+++ family_tree/names.js
@@ -42,7 +42,8 @@ function init() {
         var str = "";
         //if (person.birthYear !== undefined) str += "Born: " + person.birthYear;
         //if (person.deathYear !== undefined) str += "\nDied: " + person.deathYear;
-        if (person.married !== undefined) str += "\Married: " + person.married;
+        if (person.married !== undefined) str += "Married: " + person.married;
+		if (person.mother !== undefined) str += "\nMaother: " + person.mother;
         return str;
       }
 
@@ -96,43 +97,71 @@ function init() {
 
       // here's the family data
       var nodeDataArray = [
+	  // marceline - marcelina
         { key: 0, name: "Cano - Daliw", gender: "M", married: "Daliw" },
-			{ key: 1, parent: 0, name: "Marceline", gender: "F", married: "Arcenio Sr." },
-				{ key: 3, parent: 1, name: "Elfred (Cano)", gender: "M", married: "Rosmarie" },
+			{ key: 2, parent: 0, name: "Marceline", gender: "F", married: "Arcenio Sr." },
+				{ key: 3, parent: 2, name: "Elfred (Cano)", gender: "M", married: "Rosmarie" },
 					{ key: 9, parent: 3, name: "Walt", gender: "M", married: "Susef" },
-						{ key: 31, parent: 9, name: "Placeholder", gender: "M" },
+						{ key: 31, parent: 9, name: "Redrick", gender: "M" },
 						{ key: 32, parent: 9, name: "Haseo", gender: "M" },
 						{ key: 33, parent: 9, name: "Jihoon", gender: "M" },
 					{ key: 10, parent: 3, name: "Lylmar Eiramsor (Gosipeng)", gender: "F" },
-						{ key: 34, parent: 10, name: "(Ramboy)", gender: "M" },
+						{ key: 34, parent: 10, name: "Dwayne Las Marias (Ramboy)", gender: "M" },
 					{ key: 11, parent: 3, name: "Dana Xanthea (Ibing)", gender: "F", married: "Noue" },
-						{ key: 35, parent: 11, name: "Thea", gender: "F" },
-				{ key: 4, parent: 1, name: "Placeholder", gender: "M", married: "Elsa" },
-					{ key: 12, parent: 4, name: "Phiels (Konaya)", gender: "F" },
-				{ key: 5, parent: 1, name: "Faith", gender: "F", married: "Stephen" },
+						{ key: 35, parent: 11, name: "Thea Aralhee (Malipan)", gender: "F" },
+				{ key: 4, parent: 2, name: "Philbert (Ban-ang)", gender: "M" },
+					{ key: 12, parent: 4, name: "Phiels (Konaya)", gender: "F", mother: "Alfha" },
+				{ key: 5, parent: 2, name: "Faith", gender: "F", married: "Stephen" },
 					{ key: 13, parent: 5, name: "Cheryl (Cawandi)", gender: "F", married: "Darryl Kim" },
-					{ key: 14, parent: 5, name: "Levon Jordan (Lig-o)", gender: "M", married: "Ana" },
+					{ key: 14, parent: 5, name: "Levon Jordan (Lig-owen)", gender: "M", married: "Ana" },
 						{ key: 35, parent: 14, name: "Ljeana Amara (Daliw)", gender: "F" },
 					{ key: 15, parent: 5, name: "Kim Ezra (Kinaod)", gender: "M" },
-					{ key: 16, parent: 5, name: "Jacob (Taka)", gender: "M" },
-				{ key: 6, parent: 1, name: "Nelia", gender: "F", married: "Ronald Sr." },
+					{ key: 16, parent: 5, name: "Jacob (Taca)", gender: "M" },
+				{ key: 6, parent: 2, name: "Nelia", gender: "F", married: "Ronald Sr." },
 					{ key: 17, parent: 6, name: "Sherwai (Mokyat)", gender: "M" },
 					{ key: 18, parent: 6, name: "Cameron (Fagyan)", gender: "M" },
 					{ key: 19, parent: 6, name: "Basil (Cano)", gender: "M" },
 					{ key: 20, parent: 6, name: "Ronald Jr. (RJ)", gender: "M" },
-				{ key: 7, parent: 1, name: "Anastacio", gender: "M", married: "Lyn" },
+				{ key: 7, parent: 2, name: "Anastacio", gender: "M", married: "Lyn" },
 					{ key: 21, parent: 7, name: "Odali Anne", gender: "F" },
-				{ key: 8, parent: 1, name: "Arcenio Jr.", gender: "M", married: "Maralyn" },
+				{ key: 8, parent: 2, name: "Arcenio Jr.", gender: "M", married: "Maralyn" },
 					{ key: 22, parent: 8, name: "Kaye", gender: "F" },
 					{ key: 23, parent: 8, name: "Cody Craig", gender: "M" },
-			{ key: 2, parent: 0, name: "Emily", gender: "F", married: "William" },
-				{ key: 24, parent: 2, name: "Placeholder", gender: "M" },
-				{ key: 25, parent: 2, name: "Placeholder", gender: "M" },
-				{ key: 26, parent: 2, name: "Placeholder", gender: "M" },
-				{ key: 27, parent: 2, name: "Placeholder", gender: "M" },
-				{ key: 28, parent: 2, name: "Placeholder", gender: "M" },
-				{ key: 29, parent: 2, name: "Placeholder", gender: "M" },
-				{ key: 30, parent: 2, name: "Placeholder", gender: "M" }
+		// Emily			
+			{ key: 1, parent: 0, name: "Emily", gender: "F", married: "William" },
+				{ key: 24, parent: 1, name: "Nena (Ining)", gender: "F", married: "Douglas" },
+					{ key: 36, parent: 24, name: "Remedios (Bag-an)", gender: "F", married: "Dado" },
+						{ key: 54, parent: 36, name: "Rashel", gender: "F" },
+					{ key: 37, parent: 24, name: "Ricardo", gender: "M" },
+					{ key: 38, parent: 24, name: "Danny", gender: "M" },
+					{ key: 39, parent: 24, name: "Leslie Ann", gender: "F", married: "Leon" },
+						{ key: 55, parent: 39, name: "William", gender: "M" },
+					{ key: 40, parent: 24, name: "Allan", gender: "M" },
+					{ key: 40, parent: 24, name: "Nestor", gender: "M" },
+					{ key: 41, parent: 24, name: "Meady", gender: "F" },
+				{ key: 25, parent: 1, name: "Emelia", gender: "F", married: "Anthony" },
+					{ key: 42, parent: 25, name: "Dwight", gender: "M", married: "Placeholder" },
+						{ key: 56, parent: 42, name: "Placeholder", gender: "M" },
+					{ key: 43, parent: 25, name: "Eden", gender: "F", married: "Bumin" },
+						{ key: 57, parent: 43, name: "Larraine Christy", gender: "F" },
+					{ key: 44, parent: 25, name: "Pederico", gender: "M", married: "Placeholder" },
+						{ key: 58, parent: 44, name: "Eric", gender: "M" },
+						{ key: 59, parent: 44, name: "(Gatsalian)", gender: "M" },
+					{ key: 45, parent: 25, name: "Miriam", gender: "F", married: "Placeholder" },
+				{ key: 26, parent: 1, name: "Lino", gender: "M", married: "Rita" },
+					{ key: 46, parent: 26, name: "Rileen", gender: "F" },
+				{ key: 27, parent: 1, name: "Nenita", gender: "F", married: "Dennis" },
+					{ key: 47, parent: 27, name: "Bradley", gender: "M", married: "Placeholder" },
+						{ key: 60, parent: 47, name: "Placeholder", gender: "M" },
+					{ key: 48, parent: 27, name: "Camiyeng", gender: "M" },
+				{ key: 28, parent: 1, name: "Bernice", gender: "F", married: "Paul" },
+					{ key: 49, parent: 28, name: "Kevin (Willi)", gender: "M" },
+					{ key: 50, parent: 28, name: "Kristel (Ugamay)", gender: "F", married: "Placeholder" },
+					{ key: 51, parent: 28, name: "Kurt", gender: "M" },
+					{ key: 52, parent: 28, name: "Vladimir Bal", gender: "M" },
+					{ key: 53, parent: 28, name: "Krishna Suana", gender: "F" },
+				{ key: 29, parent: 1, name: "Jose", gender: "M" },
+				{ key: 30, parent: 1, name: "Eddie (Edu)", gender: "M" }
       ];
 
       // create the model for the family tree
